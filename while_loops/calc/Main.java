b69f34ac7cae4e26949797338b13de9f09edb690
kazhe09 <pnkesha09@gmail.com>
2025-11-30 19:15:54 -0500

    Using while loops
    Learned  how to use while loops in Calculator question add more things

--- while_loops/calc/Main.java
+++ while_loops/calc/Main.java
@@ -13,6 +13,8 @@ class Main{
 
         System.out.println(" 1.  Addition ");
         System.out.println (" 2. Subtraction ");
+        System.out.println (" 3. Multiplication ");
+        System.out.println(" 4. Division ");
         System.out.println( " Enter  your choice :- ");
         int choice = input.nextInt ();
 
@@ -20,11 +22,20 @@ class Main{
             System.out.println("Answer :- " + (num1 + num2 ));
         } else if ( choice == 2) {
             System.out.println(" Answer :-" + (num1 - num2 ));
+        } else if ( choice == 3) {
+            System.out.println("Answer :- " + (num1 * num2 ));
+        } else if ( choice == 4) {
+            if ( num2 != 0 ){
+                System.out.println(" Answer :- " + (num1 / num2 ));
+            } else {
+                System.out.println(" Cann't divide by zero ." );
+            }
         } else {
             System.out.println( " Invalid choice.");
         }
 
         System.out.println(" Do you want to continue (y/n) ? ");
+        input.nextLine();
         String Ans = input.nextLine();
         if ( Ans == ("n")) {
             break; 
