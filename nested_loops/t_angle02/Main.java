4506a954fd05f3479bdc6ec00fc4ff0b0906716e
kazhe09 <pnkesha09@gmail.com>
2026-01-10 11:41:18 -0500

    nested_loops
    Calculating student mark using the nested loops

--- nested_loops/t_angle02/Main.java
+++ nested_loops/t_angle02/Main.java
@@ -4,10 +4,20 @@ class Main{
   public static void main(String args[]) {
     Scanner input = new Scanner ( System.in);
 
-    for(int i = 1; i <= 5; i++){
-        for ( int n = 5; n >= i ; n--)
-            System.out.println("*");
+    int total = 5;
+
+    for(int row = 0; row < total; row++){
+      int space = row;
+      int stars = total - row;
+
+      for ( int s = 0; s < space; s++){
+        System.out.print(" ");
+      }
+      for (int c = 0; c < stars; c++){
+        System.out.print("*");
+      }
+       System.out.println();
+
     }
-    System.out.println();
   }
 }
